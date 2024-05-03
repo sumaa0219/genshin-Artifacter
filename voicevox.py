bcdb8c42fd74606f4a16d27f0aca6a9bb3f089ca
sumaa0219 <sumaa0219@gmail.com>
2024-05-03 16:02:07 +0900

    VC関連の修正

--- voicevox.py
+++ voicevox.py
@@ -13,7 +13,7 @@ def text_2_wav(text, speaker_id, max_retry=20):
     # 音声合成のための、クエリを作成
     query_payload = {"text": text, "speaker": speaker_id}
     for query_i in range(max_retry):
-        response = requests.post("http://localhost:50021/audio_query",
+        response = requests.post("http://192.168.0.200:50021/audio_query",
                                  params=query_payload,
                                  timeout=10)
         if response.status_code == 200:
@@ -21,7 +21,7 @@ def text_2_wav(text, speaker_id, max_retry=20):
             # 音声合成データの作成して、wavファイルに保存
             synth_payload = {"speaker": speaker_id}
             for synth_i in range(max_retry):
-                response = requests.post("http://localhost:50021/synthesis",
+                response = requests.post("http://192.168.0.200:50021/synthesis",
                                          params=synth_payload,
                                          data=json.dumps(query_data),
                                          timeout=40)
@@ -29,5 +29,22 @@ def text_2_wav(text, speaker_id, max_retry=20):
                     return response.content
             else:
                 raise ConnectionError('リトライ回数が上限に到達しました。')
-    else:
-        raise ConnectionError('リトライ回数が上限に到達しました。')
+        else:
+            response = requests.post("http://localhost:50021/audio_query",
+                                     params=query_payload,
+                                     timeout=10)
+            if response.status_code == 200:
+                query_data = response.json()
+                # 音声合成データの作成して、wavファイルに保存
+                synth_payload = {"speaker": speaker_id}
+                for synth_i in range(max_retry):
+                    response = requests.post("http://localhost:50021/synthesis",
+                                             params=synth_payload,
+                                             data=json.dumps(query_data),
+                                             timeout=40)
+                    if response.status_code == 200:
+                        return response.content
+                else:
+                    raise ConnectionError('リトライ回数が上限に到達しました。')
+            else:
+                raise ConnectionError('リトライ回数が上限に到達しました。')
