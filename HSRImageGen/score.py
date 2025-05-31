f412171e8460378720a55f588fd1ad44c4b928b2
sumaa0219 <sumaa0219@gmail.com>
2025-06-01 06:39:41 +0900

    変更

--- HSRImageGen/score.py
+++ HSRImageGen/score.py
@@ -4,10 +4,13 @@ import requests
 
 with open('assetData/HSR_max.json', 'r', encoding="utf-8") as json_file:
     HSR_max = json.load(json_file)
-
+    
+exCharaID = ""
+HSR_weight = {}
 
 def calculationScore(charaID: int, relicInfo: relic):
     score = 0
+    print(relicInfo)
     # メインステータスのスコア計算
     score += addScore(charaID, "main", relicInfo.level, relicInfo.type,
                       relicInfo.mainStatus.type, relicInfo.mainStatus.value)
@@ -20,27 +23,34 @@ def calculationScore(charaID: int, relicInfo: relic):
 
 
 def addScore(charaID: int, MainOrSub: str, level: int, place: str, type: str, value: float):
-
-    res = requests.get(f"https://hcs.lenlino.com/weight/{str(charaID)}")
-    if res.status_code == 200:
-        HSR_weight = res.json()
+    global exCharaID, HSR_weight
+    if exCharaID != str(charaID):
+        exCharaID = str(charaID)
+        res = requests.get(f"https://tools.jabrek.net/StarRail/api/weights/?character_ids={str(charaID)}")
+        HSR_weight = res.json()["body"][0]["weights"]
 
     # 色々変換
     place = place.lower()
-    if place == "foot":
+    if place == "1":
+        place = "head"
+    if place == "2":
+        place = "hand"
+    if place == "3":
+        place = "body"
+    if place == "4":
         place = "feet"
-    if place == "neck":
+    if place == "5":
         place = "sphere"
-    if place == "object":
+    if place == "6":
         place = "rope"
 
     if MainOrSub == "main":
-        weight = HSR_weight[MainOrSub]["w"+place][type]
+        weight = HSR_weight[place][type]
         return (level + 1) / 16 * weight * 50
     elif MainOrSub == "weight":
         if type == "CriticalChance" or type == "CriticalDamage" or type == "StatusProbability" or type == "StatusResistance" or type == "BreakDamageAddedRatio":
             type += "Base"
-        weight = HSR_weight[MainOrSub][type]
+        weight = HSR_weight["sub"][type]
         max = HSR_max[type]
         return value / max * weight * 50
 
